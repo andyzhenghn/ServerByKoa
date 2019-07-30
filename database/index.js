3d92a36bfa32801e84f62a58cf6cb5660e1356b9
zhihao.zheng <zhengzhihao994@outlook.com>
2019-07-30 17:03:11 +0800

    Import mongodb & mongoose & bcrypt

---
+++ database/index.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const glob = require('glob');
+const path = require('path');
+
+// 定义数据库地址的常量
+// 更标准的可以新建一个数据配置文件，专门存放数据相关的配置
+const DB_ADRESS = 'mongodb://localhost/ServerByKoa';
+let connectConfig = { useNewUrlParser: true };
+
+mongoose.Promise = global.Promise;
+mongoose.set('useCreateIndex', true);
+
+const lg = console.log.bind(console);
+
+// 定义连接函数
+const connect = () => {
+    // 重连次数
+    let connectTimes = 0;
+    const MAX_CONNECT_TIMES = 3;
+
+    const reconnectDB = (resolve, reject) => {
+        if (connectTimes < MAX_CONNECT_TIMES) {
+            connectTimes++;
+            mongoose.connect(DB_ADDRESS, connectConfig);
+        } else {
+            lg('[mongodb] database connect fail!');
+            reject();
+        }
+    };
+
+    // 连接数据库
+    mongoose.connect(DB_ADRESS, connectConfig);
+    
+    return new Promise((resolve, reject) => {
+        // 监听断开
+        mongoose.connection.on('disconnected', () => {
+            reconnectDB(reject);
+        });
+        // 错误
+        mongoose.connection.on('error', error => {
+            lg(error);
+            reconnectDB(reject);
+        });
+        // 开启
+        mongoose.connection.on('open', () => {
+            resolve();
+        });
+    });
+};
+
+exports.connect = connect;
+
+exports.initSchemas = () => {
+    glob.sync(path.resolve(__dirname, './schema/', '**/*.js')).forEach(require);
+};
+
---
+++ database/schema/User.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const { Schema } = mongoose;
+const SALT_ROUNDS = 10;
+
+let ObjectId = Schema.Types.ObjectId;
+
+const userSchema = new Schema({
+    UserId: ObjectId,
+    userName: {
+        unique: true,
+        type: String
+    },
+    password: String,
+    likes: {
+        type: Array,
+        default: []
+    },
+    collect:{
+        type: Array,
+        default: []
+    }
+}, {
+    // 加入该配置项，会自动生成创建时间
+    // 在文档更新时，也会自动更新时间
+    timestamps: {
+        createdAt: 'createdAt',
+        updatedAt: 'updateAt'
+    }
+});
+
+// 每次保存时进行密码加密
+userSchema.per('save', function(next) {
+    bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if (err) return next(err);
+            this.password = hash;
+            next();
+        });
+    });
+});
+
+// 定义userSchema的实例方法
+// 解密user password
+userSchema.methods = {
+    comparePassword(userPassword, passwordHash) {
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(userPassword, passwordHash, (err, res) => {
+                if (!err) return resolve(res);
+                return reject(err);
+            });
+        });
+    }
+};
+
+// 使用 mongoose发布模型
+mongoose.model('User', userSchema);
