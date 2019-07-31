a7ba721bf4293c994681d3ccd9d67ce5a7943c97
zhihao.zheng <zhengzhihao994@outlook.com>
2019-07-31 22:21:46 +0800

    Add the new dir src

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 const Router = require('koa-router');
 
 const articleRouter = require('./modules/article');
-const userRouter = require('./modules/user');
+const userRouter = require('./modules/modules/user');
 
 // 实例化 Router中间件
 const router = new Router({
